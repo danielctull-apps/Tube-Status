ec2af5d47547aaffbeeb20a48cdd16a157a5b81d
Daniel Tull <dt@danieltull.co.uk>
2020-06-28 13:29:38 +0100

    Shuffle project files

--- App/TubeStatus.swift
+++ App/TubeStatus.swift
@@ -1,14 +1,8 @@
-//
-//  Tube_StatusApp.swift
-//  Shared
-//
-//  Created by Daniel Tull on 28.06.2020.
-//
 
 import SwiftUI
 
 @main
-struct Tube_StatusApp: App {
+struct TubeStatus: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
