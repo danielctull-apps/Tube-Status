6763194626bcb9eea4c6989a9690b1641676858a
Daniel Tull <dt@danieltull.co.uk>
2020-06-28 13:24:04 +0100

    Initial Commit

---
+++ Shared/Tube_StatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tube_StatusApp.swift
+//  Shared
+//
+//  Created by Daniel Tull on 28.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct Tube_StatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
